74631e66281af59c9e5114c10dcfe06e34583e9b
grishc24 <girishpluto@gmail.com>
2025-02-08 23:17:27 +0530

    updated a Index File

--- src/sections/index.js
+++ src/sections/index.js
@@ -2,12 +2,12 @@ import Hero from "./Hero";
 import Product from "./Product";
 import Journey from "./Journey";
 import Services from "./Services";
-import Testimonials from "./Testimonials";
 import Students from "./Students";
 import KeyServices from "./KeyServices";
 import Aboutus from "./Aboutus";
 import Media from "./Media";
 import Faq from "./Faq";
+import Testimonials from "./Testimonials";
 import Subscribe from "./Subscribe";
 import Footer from "./Footer";
 
