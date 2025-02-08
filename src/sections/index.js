17a5efdedf68ac9d93f439250e1577bdd33c2aec
grishc24 <girishpluto@gmail.com>
2025-02-08 23:22:15 +0530

    updated a Index

--- src/sections/index.js
+++ src/sections/index.js
@@ -2,26 +2,26 @@ import Hero from "./Hero";
 import Product from "./Product";
 import Journey from "./Journey";
 import Services from "./Services";
+import Testimonials from "./Testimonials"; 
 import Students from "./Students";
+import KeyServices from "./KeyServices";
 import Aboutus from "./Aboutus";
 import Media from "./Media";
 import Faq from "./Faq";
-import Testimonials from "./testimonials";
-import KeyServices from "./KeyServices";
 import Subscribe from "./Subscribe";
 import Footer from "./Footer";
 
 export {
-    Hero,
-    Product,
-    Journey,
-    Services,
-    Testimonials,
-    Students,
-    KeyServices,
-    Aboutus,
-    Media,
-    Faq,
-    Subscribe,
-    Footer,
-}
+  Hero,
+  Product,
+  Journey,
+  Services,
+  Testimonials,
+  Students,
+  KeyServices,
+  Aboutus,
+  Media,
+  Faq,
+  Subscribe,
+  Footer,
+};
